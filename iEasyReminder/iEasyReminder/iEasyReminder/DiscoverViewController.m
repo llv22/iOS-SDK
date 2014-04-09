64fbcdb7f58d1779ac3163e4383c630fbcca5c90
Orlando Ding <xiandao.airs@gmail.com>
2014-04-09 23:08:49 +0800

    llv22 - for refreshed indicator

--- iEasyReminder/iEasyReminder/iEasyReminder/DiscoverViewController.m
+++ iEasyReminder/iEasyReminder/iEasyReminder/DiscoverViewController.m
@@ -28,7 +28,23 @@
 }
 
 - (void) performRefresh: (id)paramSender{
-    NSLog(@"Action method got refreshed.");
+    // Should be replaced with refreshed rotation
+    // http://stackoverflow.com/questions/2965737/replace-uibarbuttonitem-with-uiactivityindicatorview
+    UIActivityIndicatorView *activityIndicator = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(0, 0, self.navigationItem.rightBarButtonItem.width, self.navigationItem.rightBarButtonItem.width)];
+    UIBarButtonItem *activityItem = [[UIBarButtonItem alloc] initWithCustomView:activityIndicator];
+    self.navigationItem.rightBarButtonItem = activityItem;
+    [activityIndicator startAnimating];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSLog(@"Action method got refreshed.");
+        sleep(3);
+        NSLog(@"Action method got done.");
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.navigationController.navigationBar setTintColor:[UIColor whiteColor]];
+            self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh
+                                                                                                  target:self
+                                                                                                  action:@selector(performRefresh:)];
+        });
+    });
 }
 
 - (void)didReceiveMemoryWarning

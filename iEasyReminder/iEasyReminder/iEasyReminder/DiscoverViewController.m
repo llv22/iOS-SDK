892753db99c9ed12d5882e69476bce3b43e43c8b
Orlando Ding <xiandao.airs@gmail.com>
2014-04-12 22:29:01 +0800

    llv22 - for ibeancon stations list for displaying

--- iEasyReminder/iEasyReminder/iEasyReminder/DiscoverViewController.m
+++ iEasyReminder/iEasyReminder/iEasyReminder/DiscoverViewController.m
@@ -7,8 +7,30 @@
 //
 
 #import "DiscoverViewController.h"
+#import "ESTBeaconManager.h"
 
-@interface DiscoverViewController ()
+@interface DiscoverViewTableCell : UITableViewCell
+
+@end
+
+@implementation DiscoverViewTableCell
+
+// TODO : instancetype see http://clang.llvm.org/docs/LanguageExtensions.html#objective-c-features and http://blog.csdn.net/wzzvictory/article/details/16994913
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    self = [super initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:reuseIdentifier];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
+
+@interface DiscoverViewController () <ESTBeaconManagerDelegate>
+
+@property (nonatomic, strong) ESTBeaconManager *beaconManager;
+@property (nonatomic, strong) ESTBeaconRegion *region;
+@property (strong, nonatomic) NSArray *beaconsArray;
 
 - (void) performRefresh: (id)paramSender;
 
@@ -20,25 +42,35 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    // http://iphonedevsdk.com/forum/iphone-sdk-development/16536-uinavigationitem-title-rightbarbuttonitem-color-color-change.html
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
+    
+    // TODO : initialize for table
+    self.tableView.sectionHeaderHeight = 20;
+    [self.tableView registerClass:[DiscoverViewTableCell class] forCellReuseIdentifier:@"DiscoverViewTableCellIdentifier"];
+    
+    // TODO : for navigationbar and rightButtonItem event, http://iphonedevsdk.com/forum/iphone-sdk-development/16536-uinavigationitem-title-rightbarbuttonitem-color-color-change.html
     [self.navigationController.navigationBar setTintColor:[UIColor whiteColor]];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh
                                                                                           target:self
                                                                                           action:@selector(performRefresh:)];
 }
 
-- (void) performRefresh: (id)paramSender{
-    // Should be replaced with refreshed rotation
+- (void)updateDiscoverTableView {
+    // TODO : Should be replaced with refreshed rotation
     // http://stackoverflow.com/questions/2965737/replace-uibarbuttonitem-with-uiactivityindicatorview
     UIActivityIndicatorView *activityIndicator = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(0, 0, self.navigationItem.rightBarButtonItem.width, self.navigationItem.rightBarButtonItem.width)];
     UIBarButtonItem *activityItem = [[UIBarButtonItem alloc] initWithCustomView:activityIndicator];
     self.navigationItem.rightBarButtonItem = activityItem;
     [activityIndicator startAnimating];
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        NSLog(@"Action method got refreshed.");
-        sleep(3);
-        NSLog(@"Action method got done.");
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            if ([self.beaconsArray count] > 0) {
+                [self.tableView reloadData];
+            }
+            sleep(2);
+        });
         dispatch_async(dispatch_get_main_queue(), ^{
+//            [activityIndicator stopAnimating];
             [self.navigationController.navigationBar setTintColor:[UIColor whiteColor]];
             self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh
                                                                                                   target:self
@@ -47,10 +79,130 @@
     });
 }
 
+- (void) performRefresh: (id)paramSender{
+    [self updateDiscoverTableView];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    self.beaconManager = [[ESTBeaconManager alloc] init];
+    self.beaconManager.delegate = self;
+    
+    /*
+     * Creates sample region object (you can additionaly pass major / minor values).
+     *
+     * We specify it using only the ESTIMOTE_PROXIMITY_UUID because we want to discover all
+     * hardware beacons with Estimote's proximty UUID.
+     */
+    self.region = [[ESTBeaconRegion alloc] initWithProximityUUID:ESTIMOTE_PROXIMITY_UUID
+                                                      identifier:@"EstimoteSampleRegion"];
+    
+    /*
+     * Starts looking for Estimote beacons.
+     * All callbacks will be delivered to beaconManager delegate.
+     */
+    [self.beaconManager startRangingBeaconsInRegion:self.region];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    
+    /*
+     *Stops ranging after exiting the view.
+     */
+    [self.beaconManager stopRangingBeaconsInRegion:self.region];
+    [self.beaconManager stopEstimoteBeaconDiscovery];
+}
+
+- (void)dismiss
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - ESTBeaconManager delegate
+
+- (void)beaconManager:(ESTBeaconManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(ESTBeaconRegion *)region
+{
+    if (/*![self.beaconsArray isEqualToArray:beacons] && */[self.beaconsArray count] != [beacons count]) {
+        self.beaconsArray = beacons;
+        [self updateDiscoverTableView];
+    }
+}
+
+- (void)beaconManager:(ESTBeaconManager *)manager didDiscoverBeacons:(NSArray *)beacons inRegion:(ESTBeaconRegion *)region
+{
+    if (/*![self.beaconsArray isEqualToArray:beacons] && */[self.beaconsArray count] != [beacons count]) {
+        self.beaconsArray = beacons;
+        [self updateDiscoverTableView];
+    }
+}
+
+#pragma mark - Table view section source
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    if(section == 0)
+        return @"iBeacon stations";
+    
+    return nil;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 40;
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    int iBeaconsCount = 0;
+    if (self.beaconsArray != nil) {
+        iBeaconsCount = [self.beaconsArray count];
+    }
+    return iBeaconsCount;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    DiscoverViewTableCell *cell = [tableView dequeueReusableCellWithIdentifier:@"DiscoverViewTableCellIdentifier" forIndexPath:indexPath];
+    
+    /*
+     * Fill the table with beacon data.
+     */
+    ESTBeacon *beacon = [self.beaconsArray objectAtIndex:indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat:@"Major: %@, Minor: %@", beacon.major, beacon.minor];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"Distance: %.2f", [beacon.distance floatValue]];
+    cell.imageView.image = [UIImage imageNamed:@"beacon"];
+    
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	return 60;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    ESTBeacon *selectedBeacon = [self.beaconsArray objectAtIndex:indexPath.row];
+    
+//    self.completion(selectedBeacon);
 }
 
 @end

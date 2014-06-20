56f986c95ae8557d540e0ccfdfc7a9432486470f
llv22 <xiandao.airs@gmail.com>
2014-06-20 13:00:50 +0800

    llv22 - adjustment of scrollview and pagecontrol dynamically with constraints

--- iEasyReminder/iEasyReminder/iEasyReminder/IERLocateStuffViewController.h
+++ iEasyReminder/iEasyReminder/iEasyReminder/IERLocateStuffViewController.h
@@ -14,4 +14,8 @@
 @property (nonatomic, strong) NSString* strHostedCity;
 @property (nonatomic, strong) NSString* strStuffKey;
 
+//see : UIScrollView Control and page control
+@property (nonatomic, strong) IBOutlet UIScrollView* scrollView;
+@property (nonatomic, strong) IBOutlet UIPageControl* pageControl;
+
 @end
